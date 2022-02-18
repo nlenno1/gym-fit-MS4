990198d2ddc48e2228676d43182b4adcf6f3d120
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-18 17:08:20 +0000

    feat: add classes app with models and add a few class categories to db

---
+++ classes/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ClassCategory, SingleExerciseClass
+
+admin.site.register(ClassCategory)
+admin.site.register(SingleExerciseClass)
---
+++ classes/models.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from django.db import models
+
+from django.core.validators import MaxValueValidator
+
+# from profiles.models import User
+# from instructors.models import Instructor
+
+
+class ClassCategory(models.Model):
+    """ Class for the Categories of Exercise Classes Available """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    equipment_required = models.CharField(max_length=254, null=True,
+                                          blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        """ Return name string"""
+        return self.name
+
+    def get_friendly_name(self):
+        """ Return friendly_name string"""
+        return self.friendly_name
+
+
+class SingleExerciseClass(models.Model):
+    """ Class for the Categories of Exercise Classes Available """
+    category = models.ForeignKey(ClassCategory, null=True, blank=True,
+                                 on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    start_time = models.TimeField(auto_now=False, auto_now_add=False)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False)
+    duration = models.PositiveSmallIntegerField(validators=[MaxValueValidator(120)], null=True, blank=True)  # Calculated in function below
+    location = models.CharField(max_length=100)
+    instructor = models.CharField(max_length=100)  # Will become manytomany
+    # models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    token_cost = models.PositiveSmallIntegerField(validators=[MaxValueValidator(30)])
+    max_capacity = models.PositiveSmallIntegerField(validators=[MaxValueValidator(150)])
+    ability_level = models.CharField(max_length=30, choices=[('BEG', 'Beginner'), ('INT', 'Intermediate'), ('ADV', 'Advanced')], null=True, blank=True)
+    additional_notes = models.TextField(null=True, blank=True)
+
+    # To be filled
+    participants = models.CharField(max_length=100, null=True, blank=True)  # Will become manytomany
+    # models.ManyToManyField(User, through='somewhere', blank=True)
+    remaining_spaces = models.PositiveSmallIntegerField(validators=[MaxValueValidator(150)], null=True, blank=True)
