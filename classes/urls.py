6e3047fd906004995432348eedb3cf0a7cac4cfb
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-18 22:03:31 +0000

    add class_booking page with basic design elements

--- classes/urls.py
+++ classes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('classes/our_classes/', views.view_class_categories, name='view_class_categories'),
+    path('classes/our_classes/', views.view_class_categories,
+         name='view_class_categories'),
+    path('classes/class_bookings/', views.view_all_single_classes,
+         name='view_all_single_classes'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import ClassCategory
+from .models import ClassCategory, SingleExerciseClass
 
 def view_class_categories(request):
     """ A view to return all the categories"""
@@ -11,3 +11,17 @@ def view_class_categories(request):
     }
 
     return render(request, 'classes/our_classes.html', context)
+
+
+def view_all_single_classes(request):
+    """ A view to return all the single exercise classes"""
+
+    classes = SingleExerciseClass.objects.all()
+    categories = ClassCategory.objects.all()
+
+    context = {
+        'classes': classes,
+        'class_categories': categories
+    }
+
+    return render(request, 'classes/class_booking.html', context)
