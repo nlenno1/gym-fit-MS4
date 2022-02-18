e42d1494e0e4dbd8b61fd462676635268ef3d106
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-18 17:34:47 +0000

    customised admin view

--- classes/admin.py
+++ classes/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import ClassCategory, SingleExerciseClass
 
-admin.site.register(ClassCategory)
-admin.site.register(SingleExerciseClass)
+class ClassCategoryAdmin(admin.ModelAdmin):
+    """ Edit Class for Admin pages """
+    list_display = (
+        'friendly_name',
+    )
+
+class SingleExerciseClassAdmin(admin.ModelAdmin):
+    """ Edit Class for Admin pages """
+    list_display = (
+            'date',
+            'start_time',
+            'duration',
+            'category',
+            'instructor',
+            'price',
+            'max_capacity',
+            'ability_level',
+        )
+
+    ordering = ('-date', '-start_time')
+
+admin.site.register(ClassCategory, ClassCategoryAdmin)
+admin.site.register(SingleExerciseClass, SingleExerciseClassAdmin)
--- classes/models.py
+++ classes/models.py
@@ -9,6 +9,11 @@ from django.core.validators import MaxValueValidator
 
 class ClassCategory(models.Model):
     """ Class for the Categories of Exercise Classes Available """
+
+    class Meta:
+        """ Update Meta data for model """
+        verbose_name_plural = "Class Categories"
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -28,22 +33,37 @@ class ClassCategory(models.Model):
 
 class SingleExerciseClass(models.Model):
     """ Class for the Categories of Exercise Classes Available """
+
+    class Meta:
+        """ Update Meta data for model """
+        verbose_name_plural = "Single Exercise Classes"
+
     category = models.ForeignKey(ClassCategory, null=True, blank=True,
                                  on_delete=models.CASCADE)
     date = models.DateField(auto_now=False, auto_now_add=False)
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
-    duration = models.PositiveSmallIntegerField(validators=[MaxValueValidator(120)], null=True, blank=True)  # Calculated in function below
+    duration = models.PositiveSmallIntegerField(
+               validators=[MaxValueValidator(120)], null=True, blank=True)  # Calculated in function below
     location = models.CharField(max_length=100)
     instructor = models.CharField(max_length=100)  # Will become manytomany
     # models.ForeignKey(Instructor, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    token_cost = models.PositiveSmallIntegerField(validators=[MaxValueValidator(30)])
-    max_capacity = models.PositiveSmallIntegerField(validators=[MaxValueValidator(150)])
-    ability_level = models.CharField(max_length=30, choices=[('BEG', 'Beginner'), ('INT', 'Intermediate'), ('ADV', 'Advanced')], null=True, blank=True)
+    token_cost = models.PositiveSmallIntegerField(
+                validators=[MaxValueValidator(30)])
+    max_capacity = models.PositiveSmallIntegerField(
+                validators=[MaxValueValidator(150)])
+    ability_level = models.CharField(
+                    max_length=30, choices=[
+                        ('BEG', 'Beginner'),
+                        ('INT', 'Intermediate'),
+                        ('ADV', 'Advanced')],
+                        null=True, blank=True)
     additional_notes = models.TextField(null=True, blank=True)
 
     # To be filled
     participants = models.CharField(max_length=100, null=True, blank=True)  # Will become manytomany
     # models.ManyToManyField(User, through='somewhere', blank=True)
-    remaining_spaces = models.PositiveSmallIntegerField(validators=[MaxValueValidator(150)], null=True, blank=True)
+    remaining_spaces = models.PositiveSmallIntegerField(
+                       validators=[MaxValueValidator(150)],
+                       null=True, blank=True)
