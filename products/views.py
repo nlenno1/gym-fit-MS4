a34c4531611ba72dbf534ae46a170aca186d773a
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-19 18:36:19 +0000

    feat: add design to join us and matched it with class booking, process data by package type, add expires date variable to context and render on page

--- products/views.py
+++ products/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from .models import ClassAccessPackage
 
+from datetime import date, timedelta
+
 def view_class_access_packages(request):
     """ A view to return the class access packages available"""
 
     packages = ClassAccessPackage.objects.all()
 
+    tokens = []
+    unlimited = []
+
+    for package in packages:
+        if package.type == "UU":
+            unlimited.append(package)
+        else:
+            tokens.append(package)
+
+    for package in unlimited:
+        package.today_expires = date.today() + timedelta(days=package.duration)
+
     context = {
-        'packages': packages
+        'token_packages': tokens,
+        'unlimited_packages': unlimited,
     }
 
     return render(request, 'products/join_us.html', context)
