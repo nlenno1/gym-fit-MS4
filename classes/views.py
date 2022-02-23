77c9452c849ae6aadd5e46872fe5add12137d801
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-23 00:03:07 +0000

    refactor: refactor classes_this_week function and add comments

--- classes/views.py
+++ classes/views.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from operator import itemgetter
 
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
@@ -31,39 +30,42 @@ def view_single_class_category(request, category_id):
     return render(request, 'classes/class_category_details.html', context)
 
 
+def filter_classes_by_category(category_filter, classes):
+    """ Function to filter a selection of classes a category"""
+    category_filter = ClassCategory.objects.get(pk=category_filter)
+    filtered_classes = classes.filter(category=category_filter)
+    category_filter_name = category_filter.friendly_name
+    return category_filter_name, filtered_classes
+
 def classes_this_week(request):
     """ A view to return all the single exercise classes"""
 
     classes = SingleExerciseClass.objects.all()
     categories = ClassCategory.objects.all()
 
-    for item in classes:
-        item.friendly_date = item.date.strftime("%d/%m/%Y")
+    category_filter = ''
+    filter_name = ''
+    # generate the current date, list of dates for this week and a list of 
+    # classes that happen in the week that are properly formatted
+    current_date = datetime.strptime(
+                    datetime.now().strftime("%Y,%m,%d"), "%Y,%m,%d")
+    this_week = [(current_date + timedelta(days=x)).strftime(
+                "%Y-%m-%d") for x in range(7)]
+    selected_classes = classes.filter(date__gte=this_week[0], date__lt=this_week[-1])
 
-    today = datetime.now()
-    current_date = today.strftime("%Y,%m,%d")
-    current_date_temp = datetime.strptime(current_date, "%Y,%m,%d")
-    this_week = []
-    for x in range(7):
-        newdate = current_date_temp + timedelta(days=x)
-        this_week.append(newdate)
-
-    selected_classes = [item for item in classes if str(item.date) >= str(this_week[0] - timedelta(days=1)) and str(item.date) <= str(this_week[-1])]
     if request.GET:
         category_filter = request.GET['category_filter']
-
         # Check for all category option selected
         if category_filter == 'all' or category_filter == '':
-            selected_filter_name = 'all'
+            filter_name = 'all'
         else:  # Filter the classes by category
-            selected_classes = [item for item in selected_classes if str(item.category.id) == str(category_filter)]
-            selected_filter_name_list = [item.friendly_name for item in categories if str(item.id) == str(category_filter)]
-            selected_filter_name = selected_filter_name_list[0]
-
-    else:  # Set defaults and todays date to filter classes
-        category_filter = ''
-        selected_filter_name = ''
-
+            filter_name, selected_classes = filter_classes_by_category(category_filter, selected_classes)
+    # remove the seconds number from the time values
+    for item in selected_classes:
+        item.start_time = item.start_time.strftime('%H:%M')
+        item.end_time = item.end_time.strftime('%H:%M')
+    # Create a list of dates that contain classes with a friendly date
+    # formatted to display
     days_with_classes = []
     search_storage = []
     for item in selected_classes:
@@ -71,7 +73,6 @@ def classes_this_week(request):
             search_storage.append(item.date)
             days_with_classes.append({
                 'date': item.date,
-                'friendly_date': item.friendly_date,
                 'text_date': item.date.strftime("%A %d %b"),
             })
     days_with_classes_sorted = sorted(days_with_classes, key=lambda d: d['date'])
@@ -80,7 +81,7 @@ def classes_this_week(request):
         'classes': selected_classes,
         'class_categories': categories,
         'days_with_classes': days_with_classes_sorted,
-        'category_filter': selected_filter_name,
+        'category_filter': filter_name,
     }
 
     return render(request, 'classes/classes_this_week.html', context)
@@ -108,18 +109,19 @@ def filter_single_classes(request):
         if category_filter == 'all' or category_filter == '':
             category_filter = 'all'
         else:  # Filter the classes by category
-            category_filter = ClassCategory.objects.get(pk=category_filter)
-            category_filter_name = category_filter.friendly_name
-            filtered_classes = filtered_classes.filter(category=category_filter)
+            category_filter_name, filtered_classes = filter_classes_by_category(category_filter, filtered_classes)
 
     # Filter the Classes by date and order by start time
     filtered_classes = filtered_classes.filter(date=date_filter).order_by('start_time')
-
     # Check if classes displayed have happened yet
     now = datetime.now()
     for item in filtered_classes:
         if item.date.strftime("%d:%m:%Y - ") + item.start_time.strftime("%H:%M") <= now.strftime("%d:%m:%Y - %H:%M:%S"):
             item.closed = True
+    # remove the seconds number from the time values
+    for item in filtered_classes:
+        item.start_time = item.start_time.strftime('%H:%M')
+        item.end_time = item.end_time.strftime('%H:%M')
 
     context = {
         'classes': filtered_classes,
