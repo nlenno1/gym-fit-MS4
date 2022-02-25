84265e8a01cafcd7b14f4d112dece951e5394048
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-25 18:08:12 +0000

    feat: add toast messages

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
 
+from products.models import ClassAccessPackage
+from classes.models import SingleExerciseClass
 
 def view_bag(request):
     """ View to render the shopping bag """
@@ -10,11 +14,14 @@ def view_bag(request):
 def add_package_to_bag(request, item_id):
     """ View to add a package to the shopping bag """
 
+    package = get_object_or_404(ClassAccessPackage, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {'class_access_package': None,
                                     'single_classes': []})
 
     bag['class_access_package'] = item_id
+
+    messages.success(request, f'Added {package.friendly_name} to your shopping bag')
 
     request.session['bag'] = bag
 
@@ -24,13 +31,16 @@ def add_package_to_bag(request, item_id):
 def add_single_class_to_bag(request, item_id):
     """ View to add a single class to the shopping bag """
 
+    exercise_class = get_object_or_404(SingleExerciseClass, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {'class_access_package': None,
                                      'single_classes': []})
 
     if item_id not in bag['single_classes']:
         bag['single_classes'].append(item_id)
-        print("item added to bag")
+        messages.success(request, f'Added {exercise_class.category} at {exercise_class.start_time} on {exercise_class.date} to your shopping bag')
+    else:
+        messages.warning(request, f'{exercise_class.category} at {exercise_class.start_time} on {exercise_class.date} is already in your shopping bag')
 
     request.session['bag'] = bag
 
