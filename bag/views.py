737976d63bba78f9d99eb0851cc5545c08b91ff2
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-25 20:42:37 +0000

    add bag items to notifications

--- bag/views.py
+++ bag/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from products.models import ClassAccessPackage
 from classes.models import SingleExerciseClass
 
+
 def view_bag(request):
     """ View to render the shopping bag """
 
@@ -51,17 +52,20 @@ def add_single_class_to_bag(request, item_id):
 def remove_from_bag(request, product_type, item_id):
     """ View to remove items from the bag """
 
-    bag = request.session.get('bag', {'class_access_package': None,
-                                      'single_classes': []})
-    if product_type == 'package':
-        package = get_object_or_404(ClassAccessPackage, pk=item_id)
-        bag['class_access_package'] = None
-        messages.success(request, f'Removed {package.friendly_name} from your bag')
-    elif product_type == 'single_class':
-        exercise_class = get_object_or_404(SingleExerciseClass, pk=item_id)
-        bag['single_classes'].remove(item_id)
-        messages.success(request, f'Removed {exercise_class.category} at {exercise_class.start_time} on {exercise_class.date} from your bag')
+    try:
+        bag = request.session.get('bag', {'class_access_package': None,
+                                        'single_classes': []})
+        if product_type == 'package':
+            package = get_object_or_404(ClassAccessPackage, pk=item_id)
+            bag['class_access_package'] = None
+            messages.success(request, f'Removed {package.friendly_name} from your bag')
+        elif product_type == 'single_class':
+            exercise_class = get_object_or_404(SingleExerciseClass, pk=item_id)
+            bag['single_classes'].remove(item_id)
+            messages.success(request, f'Removed {exercise_class.category} at {exercise_class.start_time} on {exercise_class.date} from your bag')
 
-    request.session['bag'] = bag
-    
+        request.session['bag'] = bag
+    except Exception as err:
+        messages.error(request, f"Error removing item: {err}")
+
     return redirect(reverse('view_bag'))
