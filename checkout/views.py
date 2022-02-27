3664c9d57158c08235b012ac3e50afa4df415d2c
nlenno1 <nick.lennon@hotmail.co.uk>
2022-02-27 13:25:43 +0000

    add stripe payment elements with custom js elements and css styling

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KXTSbCu2tiFYO49Y8LdZAtdhoxIZM1PWn6cOnx4hGJ4idl8KlUQvqjXDouuwnnCFH6l1rWOyjyjSVXB5p7MfUeu00k5OPvskJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
