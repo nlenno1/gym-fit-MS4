31acdf85c23cd38a22c198fdf7593a1decd080a2
nlenno1 <nick.lennon@hotmail.co.uk>
2022-03-02 22:54:23 +0000

    admin: add fields to profiles admin, register model and change verbose plural for User Profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    """ Class to allow admin to view orders """
+    model = UserProfile
+    readonly_fields = ('user',)
+
+    fields = ('user', 'default_phone_number', 'default_street_address1', 'default_street_address2',
+              'default_town_or_city', 'default_county', 'default_postcode',
+              'default_country', 'dob', 'health_conditions',
+              'active_class_package', 'class_package_type', 'class_tokens',
+              'package_expiry',)
+
+    list_display = ('user', 'dob', 'active_class_package',
+                    'default_phone_number',)
+
+    ordering = ('user',)
+
+admin.site.register(UserProfile, UserProfileAdmin)
