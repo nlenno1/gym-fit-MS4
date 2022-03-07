c08a09f409dceadc5edc4924540aa762ff2d1cec
nlenno1 <nick.lennon@hotmail.co.uk>
2022-03-07 18:13:31 +0000

    feat: add create instructor profile functionality

--- instructors/views.py
+++ instructors/views.py
@@ -1,24 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from.models import Instructor
 from .forms import InstructorForm
-
-@login_required
-def add_an_instructor(request):
-    """ View to allow admin to add an instructor """
-
-    if not request.user.is_superuser:
-        messages.error(request, "Sorry, only Admin allowed")
-        return redirect(reverse('home'))
-
-    form = InstructorForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'instructors/add_an_instructor.html', context)
 
 
 @login_required
@@ -37,3 +22,35 @@ def instructor_management(request):
     }
 
     return render(request, 'instructors/instructor_management.html', context)
+
+
+@login_required
+def add_an_instructor(request):
+    """ View to allow admin to add an instructor """
+
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only Admin allowed")
+        return redirect(reverse('home'))
+
+    form = InstructorForm()
+
+    if request.method == "POST":
+        form = InstructorForm(request.POST)
+        if form.is_valid():
+            instructor = form.save(commit=False)
+            instructor.name = instructor.friendly_name.lower().replace(" ", "-")
+            instructor.friendly_name = instructor.friendly_name.title()
+            instructor.save()
+            messages.success(request, f"Instructor Profile for \
+                            {instructor.friendly_name} created")
+            return redirect(reverse('instructor_management'))
+        else:
+            messages.error(request, "Unable to Instructor Profile. Please check \
+                that the form is valid")
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'instructors/add_an_instructor.html', context)
+
