7e4dce4324bee4595230a83077299956df8f6cee
nlenno1 <nick.lennon@hotmail.co.uk>
2022-03-07 15:53:06 +0000

    feat: add delete message functionality and added contact email to messages display

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
      path('', views.send_contact_message, name='send_contact_message'),
+     path('<message_id>/delete', views.delete_contact_message, name='delete_contact_message'),
+
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .forms import ContactMessageForm
+from .models import ContactMessage
 
 
 def send_contact_message(request):
-    """ View to allow users to create a review """
+    """ View to allow users to send a message to admin """
 
     form = ContactMessageForm()
 
@@ -27,3 +29,22 @@ def send_contact_message(request):
     }
 
     return render(request, 'contact/contact.html', context)
+
+
+@login_required
+def delete_contact_message(request, message_id):
+    """ View to allow admin to delete a message """
+
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only Admin allowed")
+        return redirect(reverse('home'))
+
+    message = None
+    message = ContactMessage.objects.get(id=message_id)
+    if message:
+        message.delete()
+        messages.success(request, "Message Deleted")
+    else:
+        messages.error(request, "Could not Delete the Message")
+
+    return redirect(reverse('profile'))
