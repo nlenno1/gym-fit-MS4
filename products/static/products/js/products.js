caa68277e9455d76c021d7cc10b9f9d041cc25f3
nlenno1 <nick.lennon@hotmail.co.uk>
2022-03-09 19:12:09 +0000

    feat: add js for disabling form inputs when not required

---
+++ products/static/products/js/products.js
@@ -0,0 +1,8 @@
+$('#id_type').change(function () {
+    if (this.value == "UU"){
+        $("#id_amount_of_tokens").prop('disabled', true);
+        $("#id_amount_of_tokens").val('');
+    } else {
+        $("#id_amount_of_tokens").prop('disabled', false);
+    }
+})  
