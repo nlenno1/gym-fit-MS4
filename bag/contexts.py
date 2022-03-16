8891df6dcb04c7c7c59f7cce0e3ff237f5383a9a
nlenno1 <nick.lennon@hotmail.co.uk>
2022-03-16 16:48:32 +0000

    add background color to closed classes with a message in bag, classes this week and classes by day

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 from django.shortcuts import get_object_or_404
 from products.models import ClassAccessPackage
 from classes.models import SingleExerciseClass
@@ -31,6 +31,14 @@ def bag_contents(request):
         total += exercise_class_object.price
         product_count += 1
         bag_items["single_classes"].append(exercise_class_object)
+
+    # Check if classes in the bag have happened yet
+    now = datetime.now()
+    for item in bag_items["single_classes"]:
+        if item.class_date.strftime("%d:%m:%Y - ") + item.start_time.strftime(
+            "%H:%M"
+        ) <= now.strftime("%d:%m:%Y - %H:%M:%S"):
+            item.closed = True
 
     context = {
         "bag": bag,
