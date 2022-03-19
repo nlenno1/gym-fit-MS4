aeefaabeedd88f1ed95bb26df2f4deca9cd20909
nlenno1 <nick.lennon@hotmail.co.uk>
2022-03-19 13:28:50 +0000

    doc/refactor: refactor CAP form input control JS and  add comment to script element in html file

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -1,4 +1,5 @@
-// Disable input for amount of tokens if Unlimited Use selected 
+// Disable/Enable and control the value of the "amount of
+// tokens" field due to the selection of the package type input
 $('#id_type').change(function () {
     if (this.value == "UU"){
         $("#id_amount_of_tokens").prop('disabled', true);
@@ -7,3 +8,12 @@ $('#id_type').change(function () {
         $("#id_amount_of_tokens").prop('disabled', false);
     }
 });
+
+$('document').ready(function () {
+    if ($('#id_type')[0].value == "UU"){
+        $("#id_amount_of_tokens").prop('disabled', true);
+        $("#id_amount_of_tokens").val('');
+    } else {
+        $("#id_amount_of_tokens").prop('disabled', false);
+    }
+})
